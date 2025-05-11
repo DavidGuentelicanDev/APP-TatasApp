62e91f67cc135e0f83abc4381e725e02a7610ec1
David Guentelican <david.guentelican@gmail.com>
2025-05-11 16:41:25 -0400

    agregado metodo para editar contraseña, con validaciones, alerts y loading

--- src/app/pages/editar-contrasena/editar-contrasena.page.ts
+++ src/app/pages/editar-contrasena/editar-contrasena.page.ts
@@ -7,6 +7,7 @@ import { NavigationExtras, Router } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { IonicModule } from '@ionic/angular';
 import { AlertController, LoadingController } from '@ionic/angular';
+import { ContrasenaEditar } from 'src/app/interfaces/usuario';
 
 
 @Component({
@@ -24,6 +25,11 @@ export class EditarContrasenaPage implements OnInit {
   correoValido: boolean | null = null;
   nuevaContrasena: string = "";
   confirmarContrasena: string = "";
+
+  datosContrasena: ContrasenaEditar = {
+    id: 0,
+    contrasena: ""
+  };
 
   constructor(
     private apiConfig: ApiConfigService,
@@ -76,6 +82,102 @@ export class EditarContrasenaPage implements OnInit {
       await loading.dismiss();
       this.correoValido = this.correoIngresado.trim().toLowerCase() === this.correoUsuario.trim().toLowerCase();
     }, 500);
+  }
+
+  //metodo para editar contraseña
+  //creado por david el 11/05
+  async editarContrasena() {
+    this.datosContrasena = {
+      id: this.idUsuarioLogueado,
+      contrasena: this.nuevaContrasena
+    };
+
+    if (!this.nuevaContrasena || !this.confirmarContrasena) {
+      console.log("tatas TODOS LOS CAMPOS SON OBLIGATORIOS");
+      await this.alertValidaciones("Error", "Debes ingresar ambas contraseñas para poder guardar");
+      return;
+    }
+
+    const formatoPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!formatoPassword.test(this.nuevaContrasena)) {
+      console.log("tatas FORMATO DE CONTRASEÑA NO VÁLIDO");
+      await this.alertValidaciones("Error", "Recuerda que tu contraseña debe tener mínimo 8 caractéres (una Mayúscula, una minúscula y un número");
+      this.nuevaContrasena = "";
+      this.confirmarContrasena = "";
+      return;
+    }
+
+    if (this.nuevaContrasena != this.confirmarContrasena) {
+      console.log("tatas LAS CONTRASEÑAS NO COINCIDEN");
+      await this.alertValidaciones("Error", "Las contraseñas ingresadas no coinciden. Ingrésalas nuevamente");
+      this.confirmarContrasena = "";
+      return;
+    }
+
+    //falta validar que el correo no este registrado previamente
+
+    let loading = await this.loadingController.create({
+      message: 'Actualizando tu contraseña...',
+      spinner: 'crescent',
+      backdropDismiss: false,
+    });
+    await loading.present();
+
+    try {
+      let datos = this.apiConfig.editarContrasena(this.datosContrasena);
+      let respuesta = await lastValueFrom(datos);
+      let json_texto = JSON.stringify(respuesta);
+      let json = JSON.parse(json_texto);
+
+      await loading.dismiss();
+
+      if (json.status === "success") {
+        console.log("tatas", json.message);
+
+        let alertaExito = await this.alertController.create({
+          header: "Éxito",
+          message: "Contraseña actualizada correctamente. Debes volver a ingresar tus credenciales",
+          backdropDismiss: false
+        });
+        await alertaExito.present();
+
+        setTimeout(async () => {
+          await alertaExito.dismiss();
+          await this.cerrarSesion();
+        }, 1500);
+      } else {
+        console.log("tatas", json.message);
+        await this.alertValidaciones("Error", json.message);
+      }
+    } catch (e) {
+      console.error("tatas ERROR AL INTENTAR EDITAR LA CONTRASEÑA: ", JSON.stringify(e));
+      await this.alertValidaciones("Error", "Hubo un error al guardar los datos. Intenta nuevamente más tarde");
+    }
+  }
+
+  //alert validaciones y errores
+  //creado por david el 10/05
+  async alertValidaciones(titulo: string, mensaje: string) {
+    let alerta = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: ["OK"]
+    });
+    await alerta.present();
+  }
+
+  //metodo para cerrar sesion
+  //creado por david el 23/04
+  async cerrarSesion() {
+    let extras: NavigationExtras = {replaceUrl: true}
+    await this.borrarUsuarioLogueado(); //borrar los datos de la tabla
+    this.router.navigate(["login"], extras);
+  }
+
+  //borrar registros en la tabla local usuario
+  //creado por david el 23/04
+  async borrarUsuarioLogueado() {
+    await this.dbOff.borrarUsuarioLogueado();
   }
 
 }
